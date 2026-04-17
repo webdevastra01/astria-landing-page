a32174714a23d0cb3ac3b539c14b406f0b93ed44
CodingMantis <k.tan.531880@umindanao.edu.ph>
2026-04-17 15:53:23 +0800

    feat(ui): solution section

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navigationbar";
 import ProblemSection from "./components/ProblemSection";
 import SocialProofBar from "./components/SocialProofBar";
+import SolutionSection from "./components/SolutionSection";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-    <HeroSection/>
-    <SocialProofBar/>
-    <ProblemSection/>
+      <Navbar />
+      <HeroSection />
+      <SocialProofBar />
+      <ProblemSection />
+      <SolutionSection />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
